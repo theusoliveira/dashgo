3367f8e289fb691745094ab34dd44db7274df15a
Matheus Oliveira <matheus.oliveira@ivoryit.com.br>
2021-04-09 13:46:52 -0300

    Form validation signIn

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,19 +1,25 @@
-import { ReactElement } from 'react';
+import { ForwardRefRenderFunction, forwardRef } from 'react';
+import { FieldError } from 'react-hook-form';
 import {
   Input as ChakraInput,
   FormLabel,
   FormControl,
   InputProps as ChakraInputProps,
+  FormErrorMessage,
 } from '@chakra-ui/react';
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
+  error?: FieldError;
 }
 
-export function Input({ name, label, ...rest }: InputProps): ReactElement {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, label, error = null, ...rest },
+  ref,
+) => {
   return (
-    <FormControl>
+    <FormControl isInvalid={!!error}>
       {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
       <ChakraInput
         name={name}
@@ -25,8 +31,13 @@ export function Input({ name, label, ...rest }: InputProps): ReactElement {
           bgColor: 'gray.900',
         }}
         size="lg"
+        ref={ref}
         {...rest}
       />
+
+      {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
     </FormControl>
   );
-}
+};
+
+export const Input = forwardRef(InputBase);
