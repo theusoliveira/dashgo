1ebd085a5641097c5d2d43c61b8991fda2083646
Matheus Oliveira <matheus.oliveira@ivoryit.com.br>
2021-04-13 08:28:56 -0300

    react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { ReactElement } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { theme } from '../styles/theme';
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 import { makeServer } from '../services/mirage';
@@ -9,13 +10,17 @@ if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
 
